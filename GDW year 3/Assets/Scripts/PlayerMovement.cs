e2f3e6ee0b0a068e6cc9e4239283583c17a092a1
APichu-afk <56273513+APichu-afk@users.noreply.github.com>
2021-09-26 19:57:11 -0400

    2021-09-26 WebGL Build
    First WebGL version

--- GDW year 3/Assets/Scripts/PlayerMovement.cs
+++ GDW year 3/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController player;//gets the character controller
     public float speed = 5.0f;//speed of the character movement
+    public float gravity = 15.0f;//Gravity intensity
+    private Vector3 movementDirection = Vector3.zero;
     // Update is called once per frame
     void Update()
     {
-        float hori = Input.GetAxis("Horizontal");//Geting input for the x axis
-        float vert = Input.GetAxis("Vertical");//Getimg input for the z axis
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movementDirection = transform.TransformDirection(movementDirection);
 
-        player.Move((transform.right * hori + transform.forward * vert)* speed * Time.deltaTime);//Movement on the x and z axis
+        movementDirection *= speed;
+
+        movementDirection.y -= gravity;
+
+        player.Move(movementDirection * Time.deltaTime);
+
+
+        //float hori = Input.GetAxis("Horizontal");//Geting input for the x axis
+        //float vert = Input.GetAxis("Vertical");//Getimg input for the z axis
+
+        //player.Move((transform.right * hori + transform.forward * vert)* speed * Time.deltaTime);//Movement on the x and z axis
     }
 }
